9b8902a9c4811f68748a38e87d78a92eb6391cbb
Fabrice Payet <hello@fabricepayet.fr>
2019-07-15 17:59:38 +0400

    Adding express and a binding port for heroku monitoring

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const Telegraf = require("telegraf");
 const dotenv = require("dotenv");
+
+const express = require("express");
+const app = express();
+
 dotenv.config();
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
@@ -9,4 +13,10 @@ bot.on("sticker", ctx => ctx.reply("👍"));
 bot.hears("hi", ctx => ctx.reply("Hey there"));
 bot.launch();
 
-console.log("AlignakBot is ready!!");
+app.listen(3000);
+
+app.get("/", function(req, res) {
+  return res.json({
+    success: true
+  });
+});
